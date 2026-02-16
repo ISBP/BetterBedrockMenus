e20c001571072c1119e6f05b450dafaa707c8797
PBSI <125078014+ISBP@users.noreply.github.com>
2026-02-16 12:36:22 -0800

    Initial Commit

---
+++ src/main/java/xyz/pbsi/betterBedrockMenus/BetterBedrockMenus.java
@@ -0,0 +1,17 @@
+package xyz.pbsi.betterBedrockMenus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterBedrockMenus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
