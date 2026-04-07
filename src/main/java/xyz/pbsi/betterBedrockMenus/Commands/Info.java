3e9b39bdffb3c82bf1e3ec6cdaac235165022bd0
PBSI <125078014+ISBP@users.noreply.github.com>
2026-04-06 19:29:28 -0700

    Cleaned up and expanded plugin info command, and fixed missing permission check

--- src/main/java/xyz/pbsi/betterBedrockMenus/Commands/Info.java
+++ src/main/java/xyz/pbsi/betterBedrockMenus/Commands/Info.java
@@ -15,10 +15,24 @@ public class Info implements CommandExecutor, TabCompleter {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
         if(args.length > 0 && args[0].equals("reload"))
         {
+            if(!sender.hasPermission("bbm.admin"))
+            {
+                sender.sendMessage("§cNo permission!");
+                return true;
+            }
             BetterBedrockMenus.getInstance().reloadConfig();
             sender.sendMessage("§aReloaded the config!");
+
         }else {
-            sender.sendMessage("§bBetter Bedrock Menus\n§bCreated by: §3pbsi");
+            String version = BetterBedrockMenus.getInstance().getPluginMeta().getVersion();
+            if(version.contains("beta"))
+            {
+                sender.sendMessage("§fBetter §aBedrock §fMenus §8(§7Beta / Internal§8)\n§8 | §fDeveloper: §apbsi\n§8 | §fVersion: §a"+version+"\n§8 | §fAPI Ver: §a"+BetterBedrockMenus.getInstance().getPluginMeta().getAPIVersion());
+
+            }
+            else {
+                sender.sendMessage("§fBetter §aBedrock §fMenus §8(§7Prod§8)\n§8 | §fDeveloper: §apbsi\n§8 | §fVersion: §a"+version+"\n§8 | §fAPI Ver: §a"+BetterBedrockMenus.getInstance().getPluginMeta().getAPIVersion());
+            }
         }
         return true;
     }
