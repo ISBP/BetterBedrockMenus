6b0c1d6e56a5d809545664adf182285125abb9ff
PBSI <125078014+ISBP@users.noreply.github.com>
2026-04-19 01:05:53 -0700

    Added javadoc comments

--- src/main/java/xyz/pbsi/betterBedrockMenus/Utils/TextFormatter.java
+++ src/main/java/xyz/pbsi/betterBedrockMenus/Utils/TextFormatter.java
@@ -7,6 +7,12 @@ import org.bukkit.entity.Player;
 public class TextFormatter {
     //This function replaces the & in messages with §, as you can't type a section symbol within Minecraft java
     //It will only replace it if the following character is not a space, so you can use & normally too
+
+    /**
+     * Formats color codes of a string. Will only replace the symbol if the character following the & symbol isn't a space
+     * @param message The message to format
+     * @return The message with color codes format
+     */
     public String formatColorCodes(String message)
     {
         StringBuilder newMessage = new StringBuilder(message);
@@ -23,6 +29,12 @@ public class TextFormatter {
         return newMessage.toString();
     }
 
+    /**
+     * Formats the placeholders in a string from the perspective of a player via Placeholder API
+     * @param text The string to be formatted
+     * @param player The player who to format from the perspective of
+     * @return The string with placeholders formatted
+     */
     public String formatPlaceholders(String text, Player player)
     {
         if(Bukkit.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI"))
@@ -32,6 +44,12 @@ public class TextFormatter {
         return text;
     }
 
+    /**
+     * Formats both color codes and placeholders for a string
+     * @param text The text to format
+     * @param player The player to format the placeholder's perspective from
+     * @return The fully formatted string
+     */
     public String fullTextFormat(String text, Player player)
     {
         return formatPlaceholders(formatColorCodes(text), player);
